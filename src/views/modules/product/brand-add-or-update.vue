27c3fa1d69f9774cb5bbddc805e05ad01b951103
zhanghanxiao <mailblankpage@163.com>
2024-03-05 13:10:15 +0800

    表单校验完成

--- src/views/modules/product/brand-add-or-update.vue
+++ src/views/modules/product/brand-add-or-update.vue
@@ -60,9 +60,27 @@ export default {
         name: "",
         logo: "",
         descript: "",
-        showStatus: "",
+        showStatus: 1,
         firstLetter: "",
-        sort: "",
+        sort: 0,
+        validateFirstLetter(rule, value, callback) {
+          if (value == "") {
+            callback(new Error("首字母必须填写"));
+          } else if (!/^[a-zA-Z]$/.test(value)) {
+            callback(new Error("首字母必须在a-z或者A-Z之间"));
+          } else {
+            callback;
+          }
+        },
+        validateSort(rule, value, callback) {
+          if (value == "") {
+            callback(new Error("排序必须填写"));
+          } else if (!Number.isInteger(value) || value < 0) {
+            callback(new Error("排序必须是一个大于等于0的整数"));
+          } else {
+            callback;
+          }
+        },
       },
       dataRule: {
         name: [{ required: true, message: "品牌名不能为空", trigger: "blur" }],
@@ -80,9 +98,32 @@ export default {
           },
         ],
         firstLetter: [
-          { required: true, message: "检索首字母不能为空", trigger: "blur" },
+          // { required: true, message: "检索首字母不能为空", trigger: "blur" },
+          {
+            validator: (rule, value, callback) => {
+              if (value == "") {
+                callback(new Error("首字母必须填写"));
+              } else if (!/^[a-zA-Z]$/.test(value)) {
+                callback(new Error("首字母必须在a-z或者A-Z之间"));
+              } else {
+                callback;
+              }
+            },
+            trigger: "blur",
+          },
         ],
-        sort: [{ required: true, message: "排序不能为空", trigger: "blur" }],
+        sort: [
+          // { required: true, message: "排序不能为空", trigger: "blur" }
+          { validator:(rule, value, callback) =>{
+          if (value == "") {
+            callback(new Error("排序必须填写"));
+          } else if (!Number.isInteger(value) || value < 0) {
+            callback(new Error("排序必须是一个大于等于0的整数"));
+          } else {
+            callback;
+          }
+        }, trigger: "blur" },
+        ],
       },
     };
   },
